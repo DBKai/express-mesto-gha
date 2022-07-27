6ee2636df5d6dfbb2caf9f4492ed7b66e6449ea9
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-07-27 19:58:37 +0500

    feat: added mongoose

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 app.get('/', (req, res) => {
   res.send('Hello world');
